97e5d9675e2ef9f607689a9c56b649ae41823a68
gomerlavado <gdlavado@gmail.com>
2024-01-01 19:01:30 -0500

    Add power and kirchhoff

--- phasors.py
+++ phasors.py
@@ -4,32 +4,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Single phase system
-v_rms= 220
-i_rms = 1
-v_max = math.sqrt(2) * v_rms
-i_max = math.sqrt(2) * i_rms
-theta_v = 60
-theta_i = 30
+vrms = 220
+irms = 1
+vm = math.sqrt(2) * vrms
+im = math.sqrt(2) * irms
+thetav = math.radians(60)
+thetai = math.radians(30)
 omega = 2 * math.pi * 60
-t = np.linspace(0, 0.1, 1000)
+time = np.linspace(0, 0.1, 1000)
 
-vt = v_max * np.cos(omega * t + math.radians(theta_v))
-it = i_max * np.cos(omega * t + math.radians(theta_i))
+vt = vm * np.cos(omega * time + thetav)
+it = im * np.cos(omega * time + thetai)
 
-plt.plot(t, vt, "r-", t, it, "b-", t, vt*it, "g-")
-plt.hlines(220, 0, 0.1, colors="r", linestyles="dashed")
+plt.plot(time, vt, "orange", time, it, "tab:green", time, vt*it, "tab:blue")
+plt.hlines(220, 0, 0.1, colors="orange", linestyles="dashed")
 plt.show()
 
-phi = theta_v - theta_i
-p_avg = 0.5 * v_max * i_max * math.cos(math.radians(phi))
-print(p_avg)
+phi = thetav - thetai
+pavg = vrms * irms * math.cos(phi)
+print(pavg)
 
-# Complex voltage, current and power
-cv = complex(v_rms * math.cos(math.radians(theta_v)), v_rms * math.sin(math.radians(theta_v)))
-ci = complex(i_rms * math.cos(math.radians(theta_i)), i_rms * math.sin(math.radians(theta_i)))
+vz = complex(vrms * math.cos(thetav), vrms * math.sin(thetav))
+iz = complex(irms * math.cos(thetai), irms * math.sin(thetai))
 
-cp = cv*ci.conjugate()
-print(cp)
-print(cmath.polar(cp))
+sz = vz * iz.conjugate()
+print(sz)
+print(sz.real)
+print(cmath.polar(sz))
 
-power_factor = math.cos(math.radians(phi))
+pf = math.cos(phi)
+print(math.acos(pf))
